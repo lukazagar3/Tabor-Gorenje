92b36a6f49143f0d28d481d2753333704d17f7d0
Luka Zagar <luka.zagar.3@gmail.com>
2025-08-10 15:32:48 +0200

    Prvotno stanje

--- random_število.py
+++ random_število.py
@@ -1,3 +1,7 @@
 import random
-število = random.randint(1, 10)
-print(število)
+št1str = input("Vpišite spodnjo mejo obsega števil, ki jih bo računalnik lahko izbral: ")
+št1int = int(št1str)
+št2str = input("Vpišite zgornjo mejo obsega števil, ki jih bo računalnik lahko izbral: ")
+št2int = int(št2str)
+število = random.randint(št1int, št2int)
+print (f"Naključno število je: {število}")
